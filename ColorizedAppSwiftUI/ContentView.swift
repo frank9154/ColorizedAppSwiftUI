d2bc7ca737b753f7d170adf05afa1afde7afedb9
alexsokolov <frank9154@gmail.com>
2024-03-06 01:47:30 +0300

    Fifth commit

--- ColorizedAppSwiftUI/ContentView.swift
+++ ColorizedAppSwiftUI/ContentView.swift
@@ -12,40 +12,37 @@ struct ContentView: View {
     @State private var greenValue = Double.random(in: 0...255)
     @State private var blueValue = Double.random(in: 0...255)
 
-        var body: some View {
-            VStack {
-                Color(red: redValue/255, green: greenValue/255, blue: blueValue/255)
-                    .frame(width: 360, height: 140)
-                    .padding()
+    var body: some View {
+          VStack {
+              Color(red: redValue/255, green: greenValue/255, blue: blueValue/255)
+                  .frame(width: 360, height: 140)
+                  .overlay(RoundedRectangle(cornerRadius: 15)
+                      .stroke(.white, lineWidth: 4))
+                  
+              Spacer()
+
+              VStack {
+                  Text("Red: \(Int(redValue))").padding(.horizontal)
+                  Slider(value: $redValue, in: 0...255, step: 1.0)
+                      .accentColor(.red)
+                 
+                  Text("Green: \(Int(greenValue))").padding(.horizontal)
+                  Slider(value: $greenValue, in: 0...255, step: 1.0)
+                      .accentColor(.green)
+                  
+                  Text("Blue: \(Int(blueValue))").padding(.horizontal)
                 
-                HStack {
-                    Text("Red: \(Int(redValue))").padding(.horizontal)
-                    Spacer()
-                }
-                Slider(value: $redValue, in: 0...255, step: 1.0)
-                    .accentColor(.red)
-                    .padding(.horizontal)
+                  Slider(value: $blueValue, in: 0...255, step: 1.0)
+                      .accentColor(.blue)
+                    
+                  Spacer()
+              }
+          }
+          .padding()
 
-                HStack {
-                    Text("Green: \(Int(greenValue))").padding(.horizontal)
-                    Spacer()
-                }
-                Slider(value: $greenValue, in: 0...255, step: 1.0)
-                    .accentColor(.green)
-                    .padding(.horizontal)
-
-                HStack {
-                    Text("Blue: \(Int(blueValue))").padding(.horizontal)
-                    Spacer()
-                }
-                Slider(value: $blueValue, in: 0...255, step: 1.0)
-                    .accentColor(.blue)
-                    .padding(.horizontal)
-            }
-
-    }
+  }
 }
-
+//
 #Preview {
     ContentView()
 }
